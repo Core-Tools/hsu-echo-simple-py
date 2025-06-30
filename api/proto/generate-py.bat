7fe01c9081f02b3b03c94f7742d535a61d43047a
ai-core-tools <ai.core.tools@gmail.com>
2025-07-01 01:03:01 +0300

    proto stubs generation in makefile system

--- api/proto/generate-py.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-python -m grpc_tools.protoc -I. --python_out=../../lib/generated/api/proto --grpc_python_out=../../lib/generated/api/proto echoservice.proto
-python ../../lib/generated/api/proto/fix_imports.py 
- 
--- api/proto/generate-py.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python -m grpc_tools.protoc -I. --python_out=../../lib/generated/api/proto --grpc_python_out=../../lib/generated/api/proto echoservice.proto 
-python ../../lib/generated/api/proto/fix_imports.py
